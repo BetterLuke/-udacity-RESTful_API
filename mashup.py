31b59e2105032abfd4045c99a48bbe2fd8be4d3a
LUKE <luke.bei.2015@gmail.com>
2017-07-01 12:17:08 +0800

    second commit

--- mashup.py
+++ mashup.py
@@ -6,11 +6,17 @@ import sys
 import codecs
 
 def findARestaurant(mealType, location):
+	 Foursquare_CLIENT_ID = "APRMJQQN4K2CYIAQSMPQBWRTHFTWYGHBCSGMZRQOMHNFPPJR"
+	 Foursquare_CLIENT_SECRET = "HBEQC1TILOBCAHSBEC020AQPP2VRCZVWFXAPB1L1LS4O2VJW"
      latitude, longitude = getGeocodeLocation(location)
-	 url = ( % ())
+	 url = ('https://api.foursquare.com/v2/venues/search?ll=%s,%s&client_id=%s&client_secret=%s' % (latitude, longitude, Foursquare_CLIENT_ID, Foursquare_CLIENT_SECRET))
+	 h =httplib2.Http()
+	 response, content = h.request(url,"GET")
+	 response_json =json.load(response)
+	 print response_json
+
 
      
-
 
 if __name__ == '__main__':
 	findARestaurant("Pizza", "Tokyo, Japan")
